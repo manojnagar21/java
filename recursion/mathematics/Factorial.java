d59ec9381d8cfe814d1cadf65e180a6f438a656a
Mritunjay <mritunjay.sdg@gmail.com>
2024-12-31 12:39:22 +0530

    mathematics factorial using iteration ans recursion

---
+++ recursion/mathematics/Factorial.java
@@ -0,0 +1,53 @@
+package recursion.mathematics;
+import java.util.*;
+
+public class Factorial {
+    private static int factorialUsingIteration(int num) {
+        if(num == 0 || num == 1) {
+            return 1;
+        }
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    private static int factorialUsingRecursion(int num) {
+        if(num == 0 || num == 1) {
+            return 1;
+        }
+        // int fact = 1;
+        // for (int i = 1; i <= num; i++) {
+        //     fact = fact * i;
+        // }
+        return factorialUsingRecursion(num) * factorialUsingRecursion(num - 1);
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = myObj.nextInt();
+        System.out.println("Enter a Choice");
+        System.out.println(
+            "1. Factorial using iteration\n" +
+            "2. Factorial using recursion"
+        );
+        int choice = myObj.nextInt();
+        int answer = 0;
+        switch (choice) {
+            case 1:
+                System.out.println("Factorial using iteration of "+num+" is: ");
+                answer = factorialUsingIteration(num);
+                System.out.println(answer);
+                break;
+            case 2:
+                System.out.println("Factorial using recursion of "+num+" is: ");
+                answer = factorialUsingRecursion(num);
+                System.out.println(answer);
+                break;
+            default:
+                break;
+        }
+        myObj.close();
+    }
+}
