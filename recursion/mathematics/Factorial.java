b694d194f46a1802cac5a55c382ebc07e5109f6c
Mritunjay <mritunjay.sdg@gmail.com>
2025-01-07 00:32:28 +0530

    mathematics factorial using iteration ans recursion with comments

--- recursion/mathematics/Factorial.java
+++ recursion/mathematics/Factorial.java
@@ -8,10 +8,12 @@ public class Factorial {
      * @return the factorial of the number
      */
     private static int factorialUsingIteration(int num) {
+        // Base case: factorial of 0 or 1 is 1
         if(num == 0 || num == 1) {
             return 1;
         }
         int fact = 1;
+        // Loop to calculate factorial
         for (int i = 1; i <= num; i++) {
             fact = fact * i;
         }
@@ -24,17 +26,21 @@ public class Factorial {
      * @return the factorial of the number
      */
     private static int factorialUsingRecursion(int num) {
+        // Base case: factorial of 0 or 1 is 1
         if(num == 0 || num == 1) {
             return 1;
         }
-        return factorialUsingRecursion(num) * factorialUsingRecursion(num - 1);
+        // Recursive case: num * factorial of (num - 1)
+        return num * factorialUsingRecursion(num - 1);
     }
 
     public static void main(String[] args) {
         System.out.println("Hello");
         Scanner myObj = new Scanner(System.in);
+        // Prompt user to enter a number
         System.out.println("Enter a number");
         int num = myObj.nextInt();
+        // Prompt user to choose the method of calculation
         System.out.println("Enter a Choice");
         System.out.println(
             "1. Factorial using iteration\n" +
@@ -42,6 +48,7 @@ public class Factorial {
         );
         int choice = myObj.nextInt();
         int answer = 0;
+        // Switch case to handle user's choice
         switch (choice) {
             case 1:
                 System.out.println("Factorial using iteration of "+num+" is: ");
